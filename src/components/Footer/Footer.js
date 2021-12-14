9fcd610865a763b9ea5e897d7083521cf144787c
Anna Piskunova <a777nna102008@yandex.ru>
2021-12-14 14:18:26 +0300

    Адаптив карточки, поправила стили по страницам

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,25 +1,24 @@
-import React from 'react';
+import React from "react";
 
-import './Footer.css';
-
+import "./Footer.css";
 
 function Footer(props) {
-  
-  
   return (
-    <footer className ="footer">
-        <div className  = 'footer__block'>
-        <p className ="footer__logo">Доминион</p>
-        <p className ="footer__author">&copy; 2021.Пискунова Анна</p>
-        </div>
-        <ul className ="contacts">Контакты
-            <li className ="contact">8-950-035-10-35</li>
-            <li className ="contact">8-981-808-33-58</li>
-            <li className ="contact"></li>
+    <footer className="footer">
+      <div>
+        <article className="footer__block">
+          <p className="footer__logo">Доминион</p>
+          <p className="footer__author">&copy; 2021.Пискунова Анна</p>
+        </article>
+        <ul className="contacts">
+          Контакты
+          <li className="contact">8-950-035-10-35</li>
+          <li className="contact">8-981-808-33-58</li>
+          <li className="contact"></li>
         </ul>
+      </div>
     </footer>
   );
-
 }
 
 export default Footer;
