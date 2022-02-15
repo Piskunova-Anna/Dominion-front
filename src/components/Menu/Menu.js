7db01628e83d13a4e0e03621d39409c5720cc6a5
Anna Piskunova <a777nna102008@yandex.ru>
2022-02-15 16:42:48 +0300

    исправление шрифтов

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -9,10 +9,10 @@ function Menu(props) {
   return (
     <ul className = {`menu__list ${location.pathname === '/' ? ('none') : ''}`}>
       <NavLink exact to = "/" target="_self" activeClassName="menu__item_active" className = "menu__item">Главная</NavLink>
-      <NavLink to = "flats" target="_self" activeClassName="menu__item_active" className = "menu__item">Квартиры</NavLink>
-      <NavLink to = "new-flats" target="_self" activeClassName="menu__item_active" className = "menu__item none">Новостройки</NavLink>
-      <NavLink to = "houses" target="_self" activeClassName="menu__item_active" className = "menu__item none">Комнаты</NavLink>
-      <NavLink to = "news" target="_self" activeClassName="menu__item_active" className = "menu__item none">Новости</NavLink>
+      <NavLink to = "/flats" target="_self" activeClassName="menu__item_active" className = "menu__item">Квартиры</NavLink>
+      <NavLink to = "/new-flats" target="_self" activeClassName="menu__item_active" className = "menu__item none">Новостройки</NavLink>
+      <NavLink to = "/houses" target="_self" activeClassName="menu__item_active" className = "menu__item none">Комнаты</NavLink>
+      <NavLink to = "/news" target="_self" activeClassName="menu__item_active" className = "menu__item none">Новости</NavLink>
       <li className = "menu__item none">Прочее</li>
     </ul>
   )
